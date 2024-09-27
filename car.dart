8d7cc7884fae5f74ad7e8600ad95207ebb4ca9ba
ShohanuZzaman Sakil <szshakil66@gmail.com>
2024-09-27 23:42:59 +0600

    Initial commit

---
+++ car.dart
@@ -0,0 +1,13 @@
+
+class car {
+  String brand;
+  String model;
+  int year;
+
+  car(this.brand, this.model, this.year);
+
+  int carAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - year;
+  }
+}
